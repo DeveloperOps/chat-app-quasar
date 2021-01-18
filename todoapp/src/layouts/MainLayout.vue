c1a5e359704421bfb6cbb8efe0f6dd2dcf34ffb4
DeveloperOps <manidecp@gmail.com>
2021-01-18 16:58:20 +0530

    completed chat page design

--- todoapp/src/layouts/MainLayout.vue
+++ todoapp/src/layouts/MainLayout.vue
@@ -3,6 +3,7 @@
     <q-header elevated>
       <q-toolbar>
         <q-toolbar-title>
+          <q-btn v-if="$route.fullPath.includes('/chat')" color="primary" icon="arrow_back" v-go-back.single/>
           Chat App Firebase
         </q-toolbar-title>
       </q-toolbar>
--- todoapp/src/pages/Chat.vue
+++ todoapp/src/pages/Chat.vue
@@ -1,10 +1,38 @@
 <template>
   <q-page>
-    <h1>Chat Page</h1>
+
+    <div class="q-pa-md column col justify-end">
+       <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        name="me"
+        :text="['hey, how are you?']"
+        sent
+      />
+      <q-chat-message
+        name="Jane"
+        :text="['doing fine, how r you?']"
+      />
+    </div>
+    </div>
+    
+      <q-footer elevated>
+        <q-toolbar>
+          <q-input bg-color="white" class="full-width" rounded v-model="message" label="Message" dense>
+        <template v-slot:after>
+          <q-btn round dense flat icon="send" />
+        </template>
+      </q-input>
+        </q-toolbar>
+      </q-footer>
   </q-page>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      message: ''
+    }
+  }
 };
 </script>
