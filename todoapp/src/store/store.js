1ab96122db6277d16377632481bbe2af8de40966
DeveloperOps <manidecp@gmail.com>
2021-01-18 19:24:42 +0530

    completed auth actions

--- todoapp/src/store/store.js
+++ todoapp/src/store/store.js
@@ -2,7 +2,31 @@ import { auth, db } from 'boot/firebase';
 
 const state = {};
 const mutations = {};
-const actions = {};
+const actions = {
+  registerUser({}, data) {
+		firebaseAuth.createUserWithEmailAndPassword(data.email, data.password)
+			.then(response => {
+				let userId = firebaseAuth.currentUser.uid
+				firebaseDb.ref('users/' + userId).set({
+					name: data.name,
+					email: data.email,
+					online: true
+				})
+			})
+			.catch(error => {
+				console.log(error.message)
+			})
+	},
+	loginUser({}, data) {
+		firebaseAuth.signInWithEmailAndPassword(data.email, data.password)
+			.then(response => {
+				console.log(response)
+			})
+			.catch(error => {
+				console.log(error.message)
+			})		
+	} 
+};
 const getters = {};
 export default{
   state,
