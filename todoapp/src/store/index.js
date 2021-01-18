bc7f2c8d0fa51a73388b70b99543423868a412fe
DeveloperOps <manidecp@gmail.com>
2021-01-18 18:59:45 +0530

    minor fiz

--- todoapp/src/store/index.js
+++ todoapp/src/store/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import store from './store'
 Vue.use(Vuex)
-export default function (/* { ssrContext } */) {
+export default function () {
   const Store = new Vuex.Store({
     modules: {
       store
